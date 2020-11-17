467aa36cec5feec567ccf2decf62fb0a7fe73179
DylanB095 <dylan.bula@gmail.com>
2020-11-17 12:45:25 +0100

    Creation de la route Get input Search

--- router/produit.js
+++ router/produit.js
@@ -8,11 +8,13 @@ const express = require('express');
 
 //// Le routage consiste à déterminer comment une application répond à une demande client adressée à un noeud final particulier, 
 // à savoir un URI (ou chemin) et une méthode de requête HTTP spécifique (GET, POST, etc.).
-
 const produit = express.Router();
 
 const database = require('../database/database');
 
+
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 /* --------------------------------------------------------------------------------------------------------------------------------------------- */
 
 /* La définition d'itinéraire prend la structure suivante:
@@ -29,7 +31,7 @@ const database = require('../database/database');
 
 // Création du chemin, req qui contiendra toutes les valeur envoyé depuis le site internet
 
-produit.post("/Newproduit", (req, res) =>{
+produit.post("/Newproduit", (req, res) => {
     const InfoProduit = {
         nom: req.body.nom,
         description: req.body.description,
@@ -38,86 +40,86 @@ produit.post("/Newproduit", (req, res) =>{
         reference: req.body.reference,
     }
     database.produit.findOne({
-        where:{reference: req.body.reference}
-    })
-    .then(produit =>{
-        if(!produit){
-            database.produit.create(InfoProduit)
-            .then(produit =>{
-                res.json(produit)
-            })
-            .catch(err => {
-                res.json({
-                    error:"Produit non crée" + err
-                })
-            })
-        }
-        else{
-            res.json({
-                error:"Produit est deja créé "
-            })
-        }
-    })
-    .catch(err => {
-        res.json({
-            error: "erreur" + err
+            where: { reference: req.body.reference }
         })
-    })
+        .then(produit => {
+            if (!produit) {
+                database.produit.create(InfoProduit)
+                    .then(produit => {
+                        res.json(produit)
+                    })
+                    .catch(err => {
+                        res.json({
+                            error: "Produit non crée" + err
+                        })
+                    })
+            } else {
+                res.json({
+                    error: "Produit est deja créé "
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                error: "erreur" + err
+            })
+        })
 })
 
 /* ------------------------------------- Get Limit/Offset---------------------------------------------------------------------------------------------- */
 produit.get("/all/:limit/:offset", (req, res) => {
     database.produit.findAll({
-        limit: parseInt(req.params.limit),
-        offset: parseInt(req.params.offset),
-    })
-    .then(produit => {
-        if (produit) {
-            /**
-             * fait reference au express.router
-             * il envoie la reponse en json
-             */
-            res.json({
-                produit: produit
-            })
-        } else {
-            res.json({
-                error: "Produits n'existe pas dans la Database"
-            })
-        }
-    })
-    .catch(err => {
-        res.json("error" + err);
+            limit: parseInt(req.params.limit),
+            offset: parseInt(req.params.offset),
+        })
+        .then(produit => {
+            if (produit) {
+                /**
+                 * fait reference au express.router
+                 * il envoie la reponse en json
+                 */
+                res.json({
+                    produit: produit
+                })
+            } else {
+                res.json({
+                    error: "Produits n'existe pas dans la Database"
+                })
+            }
+        })
+        .catch(err => {
+            res.json("error" + err);
 
-    })
+        })
 });
 /* ------------------------------------- Get Limit---------------------------------------------------------------------------------------------- */
 produit.get("/limit/:limit", (req, res) => {
     database.produit.findAll({
-        attributes: {
-            include: [],
-    },
-        limit: parseInt(req.params.limit),
-    })
-    .then(produit => {
-        if (produit) {
-            /**
-             * fait reference au express.router
-             * il envoie la reponse en json
-             */
-            res.json({
-                produit: produit
-            })
-        } else {
-            res.json({
-                error: "Produits n'existe pas dans la Database"
-            })
-        }
-    })
-    .catch(err => {
-        res.json("error" + err);
+            attributes: {
+                include: [],
+            },
+            limit: parseInt(req.params.limit),
+        })
+        .then(produit => {
+            if (produit) {
+                /**
+                 * fait reference au express.router
+                 * il envoie la reponse en json
+                 */
+                res.json({
+                    produit: produit
+                })
+            } else {
+                res.json({
+                    error: "Produits n'existe pas dans la Database"
+                })
+            }
+        })
+        .catch(err => {
+            res.json("error" + err);
 
-    })});
+        })
+});
 /* ------------------------------------- Get findOne ---------------------------------------------------------------------------------------------- */
 
 produit.get("/findone/:nom", (req, res) => {
@@ -151,7 +153,7 @@ produit.get("/findone/:nom", (req, res) => {
 produit.get("/findAll", (req, res) => {
     database.produit.findAll({
             attributes: {
-                include:[],
+                include: [],
                 exclude: ["description"]
             }
         }).then(produit => {
@@ -168,56 +170,85 @@ produit.get("/findAll", (req, res) => {
 
 /* -------------------------------------- UPDATE --------------------------------------------------------------------------------------------- */
 
-produit.post("/MAJProduit", (req,res) =>{
+produit.post("/MAJProduit", (req, res) => {
     database.produit.findOne({
-        where: {reference: req.body.reference}
-    })
-    .then(produit => {
-        if(produit) {
-            produit.update({
-                nom: req.body.nom,
-                description: req.body.description,
-                image: req.body.image,
-                prix_achat_immediat: req.body.prix_achat_immediat,
-            })
-        }
-        else {
-            res.json({
-                error: "Mise a jour non effectuée"
-            })
-        }
-    })
-    .catch(err => {
-        res.json('error' + err)
-    })
+            where: { reference: req.body.reference }
+        })
+        .then(produit => {
+            if (produit) {
+                produit.update({
+                    nom: req.body.nom,
+                    description: req.body.description,
+                    image: req.body.image,
+                    prix_achat_immediat: req.body.prix_achat_immediat,
+                })
+            } else {
+                res.json({
+                    error: "Mise a jour non effectuée"
+                })
+            }
+        })
+        .catch(err => {
+            res.json('error' + err)
+        })
 })
 
 /* -------------------------------------- DELETE --------------------------------------------------------------------------------------------- */
 
-produit.delete("/SUP/:nom", (req,res) => {
+produit.delete("/SUP/:nom", (req, res) => {
     database.produit.findOne({
-        where: { nom:req.params.nom}
-    })
-    .then(produit => {
-        if(produit){
-            produit.destroy()
-            .then(() => {
-                res.json("Produit Supprimé")
-            })
-            .catch(err => {
-                res.json("error" + err)
-            })
-        } 
-        else {
-            res.json({
-                error:"Vous ne pouvez pas supprimé ce produit il n'existe pas dans la database"
-            })
-        }
-    })
-    .catch(err => {
-        res.json("error" + err)
-    })
+            where: { nom: req.params.nom }
+        })
+        .then(produit => {
+            if (produit) {
+                produit.destroy()
+                    .then(() => {
+                        res.json("Produit Supprimé")
+                    })
+                    .catch(err => {
+                        res.json("error" + err)
+                    })
+            } else {
+                res.json({
+                    error: "Vous ne pouvez pas supprimé ce produit il n'existe pas dans la database"
+                })
+            }
+        })
+        .catch(err => {
+            res.json("error" + err)
+        })
 })
 
+/* --------------------------- GET INPUT RECHERCHE---------------------------------------------- */
 
-module.exports= produit;
+produit.get("/Recherche/:nom", (req, res) => {
+    database.produit.findAll({
+            where: {
+                nom: {
+                    [Op.like]: '%' + req.params.nom + '%'
+                }
+            },
+        })
+        .then(produit => {
+            if (produit) {
+                /**
+                 * fait reference au express.router
+                 * il envoie la reponse en json
+                 */
+                res.json({
+                    produit: produit
+                })
+            } else {
+                res.json({
+                    error: "Produit n'existe pas dans la Database"
+                })
+            }
+        })
+        .catch(err => {
+            res.json("error" + err);
+
+        })
+});
+
+
+module.exports = produit;
